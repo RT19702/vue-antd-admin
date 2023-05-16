eb2e13d04641d8015f162a71292fa3e9fb7cc0c7
H.Y.C.Y.B.H <93920919@qq.com>
2023-05-16 15:04:43 +0800

    暂存 字典管理

--- src/utils/request.js
+++ src/utils/request.js
@@ -9,35 +9,40 @@ import { message } from 'ant-design-vue'
 const request = axios.create({
   // baseURL: 'http://iwebapi.zlhranhou.com/public/index.php',
   // baseURL: 'http://localhost:3000',
-  timeout: 5000
+  timeout: 5000,
 })
 
 // 添加请求拦截器
-request.interceptors.request.use((config) => {
-  // 进度条开始
-  nprogress.start()
-  return config
-}, (error) => {
-  return error
-})
+request.interceptors.request.use(
+  (config) => {
+    // 进度条开始
+    nprogress.start()
+    return config
+  },
+  (error) => {
+    return error
+  }
+)
 
 // 添加响应拦截器
-request.interceptors.response.use(function (res) {
-  // 进度条结束
-  nprogress.done()
+request.interceptors.response.use(
+  function (res) {
+    // 进度条结束
+    nprogress.done()
 
-  const response = res.data
-  /* if (response.status === 'success') {
+    const response = res.data
+    /* if (response.status === 'success') {
     message.success(response.msg)
   } else if (response.status === 'error') {
     message.error(response.msg)
   } */
-  return response
-
-}, function (error) {
-  console.log('err' + error) // for debug
-  message.error(error.message)
-  return Promise.reject(error)
-})
+    return response
+  },
+  function (error) {
+    console.log('err' + error) // for debug
+    message.error(error.message)
+    return Promise.reject(error)
+  }
+)
 
 export default request
--- src/views/administerList/DictionaryList.vue
+++ src/views/administerList/DictionaryList.vue
@@ -7,15 +7,38 @@
         @nodeClick="nodeClick"
       />
     </div>
-    <div class="content">
+    <div class="content" v-show="treeNodeObj.data_dictionary_id !== undefined">
       <div class="tree-node-detail">
         <div class="title">
           <a-icon type="setting" /><span>基本信息</span>
           <div class="control">
-            <div class="edit"><a-icon type="edit" />编辑</div>
+            <div class="edit" @click="editNode"><a-icon type="edit" />编辑</div>
             <div class="delete"><a-icon type="delete" />删除</div>
           </div>
         </div>
+        <a-descriptions title="">
+          <a-descriptions-item label="数据项名称" span="3">
+            {{ treeNodeObj.data_dictionary_name }}
+          </a-descriptions-item>
+          <a-descriptions-item label="数据顶关键字" span="3">
+            {{ treeNodeObj.data_dictionary_key }}
+          </a-descriptions-item>
+          <a-descriptions-item label="数据项值" span="3">
+            {{ treeNodeObj.data_dictionary_value }}
+          </a-descriptions-item>
+          <a-descriptions-item label="可否编辑" span="3">
+            {{ treeNodeObj.can_edit === 1 ? '可编辑' : '不可编辑' }}
+          </a-descriptions-item>
+        </a-descriptions>
+      </div>
+      <div class="table-data">
+        <div class="title">
+          <a-icon type="database" /><span>数据子项</span>
+          <div class="control">
+            <div class="edit" @click="editNode"><a-icon type="plus" />添加</div>
+          </div>
+        </div>
+        <a-table></a-table>
       </div>
     </div>
   </div>
@@ -38,6 +61,8 @@ export default {
       },
       treeData: [],
       treeNodeObj: {},
+      editNodeObj: {},
+      tableData: [],
     }
   },
   components: {
@@ -48,7 +73,7 @@ export default {
       dictionaryApi
         .getData({
           page: 1,
-          page_size: 10,
+          page_size: 9999,
         })
         .then((res) => {
           if (res.status === 'success') {
@@ -61,8 +86,27 @@ export default {
     nodeClick(node) {
       this.treeNodeObj = node
       console.log(node)
+      dictionaryApi
+        .getData({
+          page: 1,
+          page_size: 9999,
+          filters: JSON.stringify({
+            pid: this.treeNodeObj.data_dictionary_id,
+          }),
+        })
+        .then((res) => {
+          if (res.status === 'success') {
+            this.tableData = res.data.data
+            console.log(
+              '%c [ this.tableData ]-94',
+              'font-size:15px; background:#5145a2; color:#9589e6;',
+              this.tableData
+            )
+          }
+        })
     },
     getData() {},
+    editNode() {},
   },
   mounted() {
     this.getTree()
@@ -82,42 +126,46 @@ export default {
   height: 100%;
 }
 .content {
-  flex-grow: 1;
+  width: 100%;
+  padding: 0 30px;
   display: flex;
   flex-direction: column;
   height: 100%;
 }
 .tree-node-detail {
-  width: calc(100% - 60px);
+  width: 100%;
   margin: 0 auto;
   display: flex;
   flex-direction: column;
+  border-bottom: 1px solid #e8e8e8;
   font-size: 17px;
-  .title {
+  margin-bottom: 30px;
+}
+.title {
+  display: flex;
+  align-items: center;
+  margin-bottom: 30px;
+  span {
+    margin-left: 5px;
+    font-weight: bold;
+  }
+  .control {
     display: flex;
-    align-items: center;
-    span {
-      margin-left: 5px;
-      font-weight: bold;
+    margin-left: 10px;
+    gap: 5px;
+    .edit {
+      color: #1890ff;
+      cursor: pointer;
     }
-    .control {
+    .delete {
+      color: #f5222d;
+      cursor: pointer;
+    }
+    div {
       display: flex;
-      margin-left: 10px;
-      gap: 5px;
-      .edit {
-        color: #1890ff;
-        cursor: pointer;
-      }
-      .delete {
-        color: #f5222d;
-        cursor: pointer;
-      }
-      div {
-        display: flex;
-        align-items: center;
-        font-size: 13px;
-        gap: 4px;
-      }
+      align-items: center;
+      font-size: 13px;
+      gap: 4px;
     }
   }
 }
