d9f51302483d3df1c99d7d58012919de2e45de9f
RT19702 <junxiongxu6@gmail.com>
2023-05-25 16:38:49 +0800

    文章API 新增

--- src/api/article.js
+++ src/api/article.js
@@ -8,3 +8,45 @@ export function getList(params) {
     params
   })
 }
+
+/* 文章启用 */
+export function enableArticle(id) {
+  return request({
+    url: `/api/admin/article/enable/${id}`,
+    method: 'POST'
+  })
+}
+
+/* 文章禁用 */
+export function disableArticle(id, data) {
+  return request({
+    url: `/api/admin/article/disable/${id}`,
+    method: 'POST',
+    data
+  })
+}
+
+/* 文章详情 */
+export function getArticle(id) {
+  return request({
+    url: `/api/admin/article/${id}`,
+    method: 'GET'
+  })
+}
+
+/* 审核通过文章 */
+export function passArticle(id) {
+  return request({
+    url: `/api/admin/article/pass/${id}`,
+    method: 'POST'
+  })
+}
+
+/* 审核驳回文章 */
+export function rejectArticle(id, data) {
+  return request({
+    url: `/api/admin/article/noPass/${id}`,
+    method: 'POST',
+    data
+  })
+}
